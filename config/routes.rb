64d4ddb758793c008fba13838251db97879c8a22
Yutian Han <kevinhangoat@gmail.com>
2019-07-30 00:19:29 -0400

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/index'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'services/show'
-  get 'services/index'
-  get 'services/create'
-  get 'services/edit'
-  get 'services/update'
+
+  resources :services do
+    resources :bookings
+  end
   post '/login', to: 'login#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
