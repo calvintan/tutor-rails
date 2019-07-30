b69334e06c824204aecf6bc24e0418b1851159f4
Yutian Han <kevinhangoat@gmail.com>
2019-07-29 23:36:56 -0400

    setup wechat login in backend

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,23 @@
 class LoginController < ApplicationController
+  URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+  def wechat_user
+    params = {
+      appId: env["APP_ID"],
+      secret: env["APP_SECRET"],
+      js_code: params[:code],
+      grant_type: authorization_code
+    }
+
+    @wechat_response ||= RestClient.get(URL, params: params)
+    @wechat_user ||= JSON.parse(@wechat_response.body)
+  end
+
+  def login
+  @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+  render json: {
+    userId: @user.id
+  }
+end
+
+
 end
