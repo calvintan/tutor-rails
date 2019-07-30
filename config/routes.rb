3517eb46b51b2f763f2e65435c551110d85da654
Yutian Han <kevinhangoat@gmail.com>
2019-07-30 01:54:08 -0400

    service controller ready for test 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     resources :bookings
   end
   post '/login', to: 'login#login'
-  toot to: "services#index"
+  root to: "services#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
