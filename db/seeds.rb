f1ae15681b07c85a25ec8814de662008dfe9131c
Zachary Goodnough <goodnough.z@gmail.com>
2019-07-30 14:06:29 +0800

    added seed file - remember to add Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Service.destroy_all if Rails.env.development?
+
+7.times do
+  u = User.create!(
+    open_id: Faker::Name.name)
+  s = Service.create!(
+    title: Faker::Educator.course_name,
+    category: Faker::Educator.subject,
+    description: Faker::TvShows::StrangerThings.quote,
+    user: u
+    )
+    Booking.create!(
+      time: Faker::Time.forward(23, :day),
+      location: Faker::Address.street_address,
+      service: s,
+      user: u
+      )
+end
