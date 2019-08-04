5dcdc51fe96c0e10c2c94e13266bd9c2d6720a4f
Calvin Tan <hello@calvin-tan.com>
2019-08-04 16:10:01 +0800

    Add show condition to display or hide services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_033604) do
+ActiveRecord::Schema.define(version: 2019_08_02_073555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2019_08_02_033604) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "difficulty"
-    t.boolean "show", default: true
     t.float "latitude"
     t.float "longitude"
+    t.boolean "show", default: true
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
